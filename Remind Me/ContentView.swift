a2c3e5c6de908a06e31bc92cda4191ba91e5a4d1
Carlos Gil <crafaelgil@gmail.com>
2021-06-16 21:34:54 +0900

    Initial Commit

---
+++ Remind Me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remind Me
+//
+//  Created by Carlos Gil on 2021/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remind Me/Remind_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remind_MeApp.swift
+//  Remind Me
+//
+//  Created by Carlos Gil on 2021/06/16.
+//
+
+import SwiftUI
+
+@main
+struct Remind_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
