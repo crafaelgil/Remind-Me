a1130894a28a4997da01147bda578c3d6153011e
Carlos Gil <crafaelgil@gmail.com>
2021-06-16 22:17:56 +0900

    Connect test data to TaskListView

--- Remind Me/Model/Task.swift
+++ Remind Me/Model/Task.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
-struct Task{
+struct Task: Identifiable{
+    var id: String = UUID().uuidString
     var title: String
     var completed: Bool
 }
 
 #if DEBUG
-let testData = [
+let testDataTasks = [
     Task(title: "Implement UI", completed: true),
     Task(title: "Connect to Firebase", completed: false),
     Task(title: "Next task", completed: false)
--- Remind Me/Views/TaskListView.swift
+++ Remind Me/Views/TaskListView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct TaskListView: View {
+    let tasks = testDataTasks
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .leading) {
+                List(tasks) { task in
+                    Image(systemName: "circle")
+                    Text(task.title)
+                        .padding()
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                        Text("Add new task")
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Tasks")
+        }
     }
 }
 
