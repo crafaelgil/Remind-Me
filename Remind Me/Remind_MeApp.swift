cc4b674a9938e82957e840f158bbd397a1c50736
Carlos Gil <crafaelgil@gmail.com>
2021-06-17 22:28:17 +0900

    Include Firebase SDK

--- Remind Me/Remind_MeApp.swift
+++ Remind Me/Remind_MeApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Remind_MeApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        authenticationService.signIn()
+      }
+    
     var body: some Scene {
         WindowGroup {
             TaskListView()
         }
     }
 }
+
